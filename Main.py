8bdd6049f92c8d84f396ade1173b2b76fc31a653
URK21CS1079 <110010648+URK21CS1079@users.noreply.github.com>
2022-11-02 22:20:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,50 +1,66 @@
-n = int(input("Enter the Range for the Array:"))
-arr = []
-for i in range(n):
-    arr.append(int(input()))
-
-search = int(input("Enter the search element in the array:"))
-flag = False
-pos = 0
-for i in range(len(arr)):
-    if(search == arr[i]):
-        pos = i
-        flag = True
-        break
-
-if(flag == True):
-    print("The Element is present in the Array index at" , pos , "And the Element is :" , arr[pos])
-else:
-    print("Element is not present in the array")
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
 
 
-def binarySearch(arr, low , high , x):
-    if(high >= low):
-
-        mid = (high + low)//2
-
-        if arr[mid] == x:
-            return mid
-        
-        elif arr[mid] >  x:
-            return binarySearch(arr , low , mid - 1 , x)
-        else:
-            return binarySearch(arr , mid + 1 , high , x)
-    
+def insert(root, new_value) -> BinaryTreeNode:
+    """If binary search tree is empty, make a new node, declare it as root and return the root.
+        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
+        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
+        Finally, return the root.
+        """
+    if root is None:
+        root = BinaryTreeNode(new_value)
+        return root
+    if new_value < root.data:
+         if root.left_child:
+            insert(root.left_child, new_value)
+         else:
+            root.left_child = BinaryTreeNode(new_value)
     else:
-        return 0
+        if root.right_child:
+            insert(root.right_child, new_value)
+        else:
+            root.right_child = BinaryTreeNode(new_value)
 
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append(int(input()))
-
-search = int(input("Enter the search element in the array:"))
-
-result = binarySearch(arr , 0 , len(arr) - 1, search)
-if(result != 0):
-    print("Element is present at the index at:" , result)
-else:
-    print("Element is not present at the array")
+def inorder(root) -> None:
+    if(root is None):
+        return
+    inorder(root.left_child)
+    print(root.data,end = " ")
+    inorder(root.right_child)
 
 
+
+def preorder(root) -> None:
+    if(root is None):
+        return
+    print(root.data , end = " ")
+    preorder(root.left_child)
+    preorder(root.right_child)
+
+
+def postorder(root) -> None:
+    if(root is None):
+        return
+    postorder(root.left_child)
+    postorder(root.right_child)
+    print(root.data , end = " ")
+
+# Do not change the following code
+input_data = input()
+flag = True
+root = None
+for item in input_data.split(', '):
+    if flag is True:
+        root = insert(None, int(item))
+        flag = False
+    else:
+        insert(root, int(item))
+inorder(root)
+print()
+preorder(root)
+print()
+postorder(root)
